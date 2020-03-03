bde4f3cbbd1cf798bd326e659cf14df39bf13596
ellievanb <54558569+ellievanb@users.noreply.github.com>
2020-03-03 17:43:39 -0500

    adds proper game ending

--- VAnBuskirk/Assets/Scripts/PlayerMovement.cs
+++ VAnBuskirk/Assets/Scripts/PlayerMovement.cs
@@ -14,7 +14,7 @@ public class PlayerMovement : MonoBehaviour
 
     public GameObject projectilePrefab;
     public Transform ShotSpawn;
-    public float ShotSpeed = 10f;
+    public float ShotSpeed = 20f;
 
     // Start is called before the first frame update
     void Start()
@@ -30,11 +30,12 @@ public class PlayerMovement : MonoBehaviour
             m_Animator.speed = 3f;
         else
             m_Animator.speed = 1f;
-        if(Input.GetKeyDown(KeyCode.Space))
-            GameObject.Instantiate(projectilePrefab, ShotSpawn.transform.position, ShotSpawn.transform.rotation);
-        Rigidbody projectileRB = projectilePrefab.GetComponent<Rigidbody>();
-        projectileRB.velocity = transform.forward * ShotSpeed;
-
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject projectile = Instantiate(projectilePrefab, ShotSpawn.transform.position, ShotSpawn.transform.rotation);
+            Rigidbody projectileRB = projectile.GetComponent<Rigidbody>();
+            projectileRB.velocity = transform.forward * ShotSpeed;
+        }
     }
     void FixedUpdate()
     {
