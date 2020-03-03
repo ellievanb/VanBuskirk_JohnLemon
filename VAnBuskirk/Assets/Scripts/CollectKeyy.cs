c89c2b3acf5f5933fd3256cd3aa4d4ced1608c36
ellievanb <54558569+ellievanb@users.noreply.github.com>
2020-03-03 13:01:40 -0500

    adds ability to collect key

---
+++ VAnBuskirk/Assets/Scripts/CollectKeyy.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectKeyy : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+}
